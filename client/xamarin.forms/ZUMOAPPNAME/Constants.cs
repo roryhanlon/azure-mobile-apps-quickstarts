62d57dc4795b6bf2455977571c3978426d9d4702
Rory Hanlon <rhanlon@leitrimcoco.ie>
2019-06-12 00:36:02 +0100

    working with .nodejs

--- client/xamarin.forms/ZUMOAPPNAME/Constants.cs
+++ client/xamarin.forms/ZUMOAPPNAME/Constants.cs
@@ -5,7 +5,7 @@ namespace ZUMOAPPNAME
 	public static class Constants
 	{
 		// Replace strings with your Azure Mobile App endpoint.
-		public static string ApplicationURL = @"http://mobileapp110619.azurewebsites.net";
+		public static string ApplicationURL = @"https://miaowmobile.azurewebsites.net/";
 	}
 }
 
